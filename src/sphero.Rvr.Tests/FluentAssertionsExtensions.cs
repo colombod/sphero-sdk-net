ac0895d20411d38f5e7f0c1158b4195611fd71bc
Diego Colombo <dicolomb@microsoft.com>
2021-10-14 17:14:27 +0100

    fix assertion
    is sequence is different, skip checksum too

--- src/sphero.Rvr.Tests/FluentAssertionsExtensions.cs
+++ src/sphero.Rvr.Tests/FluentAssertionsExtensions.cs
@@ -1,5 +1,7 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using FluentAssertions;
 using FluentAssertions.Collections;
+using FluentAssertions.Equivalency;
 
 namespace sphero.Rvr.Tests
 {
@@ -7,8 +9,51 @@ namespace sphero.Rvr.Tests
     {
         public static AndConstraint<GenericCollectionAssertions<byte[]>> BeEquivalentMessageSetTo(this GenericCollectionAssertions<byte[]> assertion, byte[][] expectation)
         {
-            return assertion.BeEquivalentTo(expectation, config: options => options.ComparingByValue<byte[]>());
+            return assertion.BeEquivalentTo(expectation, config: options => options.ComparingByValue<byte[]>().Using(new RawMessageComparer()));
         }
     }
 
+    public class RawMessageComparer: IEqualityComparer<byte[]>
+    {
+        public bool Equals(byte[]? x, byte[]? y)
+        {
+            var equal = x.Length == y.Length;
+            var shouldMatchChecksum = true;
+            var checksumIndices = new HashSet<int>
+            {
+                x.Length - 2,
+                x.Length - 3
+            };
+            if (equal)
+            {
+                for (var i = 0; i < x.Length; i++)
+                {
+                    if (i != 6 && !checksumIndices.Contains(i))
+                    {
+                        equal = x[i] == y[i];
+                    }
+                    else if(i == 6)
+                    {
+                        shouldMatchChecksum = x[i] == y[i];
+                    }
+                    else if (checksumIndices.Contains(i) && shouldMatchChecksum)
+                    {
+                        equal = x[i] == y[i];
+                    }
+
+                    if (!equal)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return equal;
+        }
+
+        public int GetHashCode(byte[] obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
 }
