d34afd939b76e3400bb3e52d0147914d0cad4300
Paul Meyer <paulmey@microsoft.com>
2021-10-14 16:56:37 +0200

    Fixed message validation in tests

---
+++ src/sphero.Rvr.Tests/FluentAssertionsExtensions.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using FluentAssertions.Collections;
+
+namespace sphero.Rvr.Tests
+{
+    public static class FluentAssertionsExtensions
+    {
+        public static AndConstraint<GenericCollectionAssertions<byte[]>> BeEquivalentMessageSetTo(this GenericCollectionAssertions<byte[]> assertion, byte[][] expectation)
+        {
+            return assertion.BeEquivalentTo(expectation, config: options => options.ComparingByValue<byte[]>());
+        }
+    }
+
+}
