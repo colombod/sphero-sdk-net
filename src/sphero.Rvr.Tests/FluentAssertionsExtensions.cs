7b95a84528c56fa8d52e2b25a96b0f2bbc9ca9ce
Diego Colombo <dicolomb@microsoft.com>
2021-10-14 17:15:39 +0100

    hide type

--- src/sphero.Rvr.Tests/FluentAssertionsExtensions.cs
+++ src/sphero.Rvr.Tests/FluentAssertionsExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using FluentAssertions;
 using FluentAssertions.Collections;
-using FluentAssertions.Equivalency;
 
 namespace sphero.Rvr.Tests
 {
@@ -11,49 +10,50 @@ namespace sphero.Rvr.Tests
         {
             return assertion.BeEquivalentTo(expectation, config: options => options.ComparingByValue<byte[]>().Using(new RawMessageComparer()));
         }
-    }
-
-    public class RawMessageComparer: IEqualityComparer<byte[]>
-    {
-        public bool Equals(byte[]? x, byte[]? y)
+        private class RawMessageComparer : IEqualityComparer<byte[]>
         {
-            var equal = x.Length == y.Length;
-            var shouldMatchChecksum = true;
-            var checksumIndices = new HashSet<int>
+            public bool Equals(byte[]? x, byte[]? y)
             {
-                x.Length - 2,
-                x.Length - 3
-            };
-            if (equal)
-            {
-                for (var i = 0; i < x.Length; i++)
+                var equal = x.Length == y.Length;
+                var shouldMatchChecksum = true;
+                var checksumIndices = new HashSet<int>
                 {
-                    if (i != 6 && !checksumIndices.Contains(i))
+                    x.Length - 2,
+                    x.Length - 3
+                };
+                if (equal)
+                {
+                    for (var i = 0; i < x.Length; i++)
                     {
-                        equal = x[i] == y[i];
-                    }
-                    else if(i == 6)
-                    {
-                        shouldMatchChecksum = x[i] == y[i];
-                    }
-                    else if (checksumIndices.Contains(i) && shouldMatchChecksum)
-                    {
-                        equal = x[i] == y[i];
-                    }
+                        if (i != 6 && !checksumIndices.Contains(i))
+                        {
+                            equal = x[i] == y[i];
+                        }
+                        else if (i == 6)
+                        {
+                            shouldMatchChecksum = x[i] == y[i];
+                        }
+                        else if (checksumIndices.Contains(i) && shouldMatchChecksum)
+                        {
+                            equal = x[i] == y[i];
+                        }
 
-                    if (!equal)
-                    {
-                        break;
+                        if (!equal)
+                        {
+                            break;
+                        }
                     }
                 }
+
+                return equal;
             }
 
-            return equal;
-        }
-
-        public int GetHashCode(byte[] obj)
-        {
-            return obj.GetHashCode();
+            public int GetHashCode(byte[] obj)
+            {
+                return obj.GetHashCode();
+            }
         }
     }
+
+  
 }
